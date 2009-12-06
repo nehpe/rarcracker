40eda702001506e065d9b97bd1a96bb882ee8c84
Steve Gricci <steve@gricci.org>
2009-12-05 20:10:08 -0700

    Steadily working towards version 0.2, you can now use '-t numeric' and '-t alpha' to crack with just numbers or letters (respectively)

--- rarcracker.py
+++ rarcracker.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python
 #
-# Python RAR Cracker -- Cracks .RAR files
+# Python RAR Cracker -- Brute force recovery of passwords for .RAR files
 # by Steve Gricci (deepcode.net)
 
-import threading
-import string
 import math
 import subprocess
 import sys
 from threading import Thread
 import time
 
+#define
+p_type = 'alphanumeric'
+p_file = ''
+
+
 class RARCrack(Thread):
-	def __init__(self, offset,count):
+	def __init__(self,offset,count):
 		Thread.__init__(self)
 		self.offset = offset
 		self.count = count
-		self.file = sys.argv[1]
 		return
 
 	def encrypt(self, N):
-		possible_chars = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-				'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
-				'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
-				'W', 'X', 'Y', 'Z']
-		radix = len(possible_chars)
-		encoded_text = ""
-		Q = math.floor(math.fabs(N))
-		remainder = "";
-		while(Q != 0):
-			remainder = Q % radix
-			newDigit = possible_chars[int(remainder)]
-			encoded_text = newDigit + encoded_text
-			Q = ((Q-remainder)/radix)
+            global p_type
+            if (p_type == "numeric"):
+                return N
+            if (p_type == "alphanumeric"):
+                possible_chars = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+			'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+			'W', 'X', 'Y', 'Z']
+            if (p_type == "alpha"):
+                possible_chars = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+			'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+			'W', 'X', 'Y', 'Z']
 
-		return encoded_text
+            radix = len(possible_chars)
+            encoded_text = ""
+            Q = math.floor(math.fabs(N))
+            remainder = "";
+            while(Q != 0):
+            	remainder = Q % radix
+            	newDigit = possible_chars[int(remainder)]
+            	encoded_text = newDigit + encoded_text
+            	Q = ((Q-remainder)/radix)
+            return encoded_text
 
 	def run(self):
 		global count
 		global current_pwd
 		global good_password
-		cmd = ""
+                global p_file
 		i = self.count
 		while(1):
 			if (good_password != None):
@@ -51,18 +63,12 @@ class RARCrack(Thread):
 			pwd = self.encrypt(i)
 			current_pwd = pwd
 			
-			try:
-				output = subprocess.Popen([r"unrar","t", "-p%s" % ( pwd ), self.file], stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0]
-				if (output.find("All OK") != -1):
-					print 'found good password: %s' % ( pwd )
-					good_password = 1
-				i += self.offset 
-			except KeyboardInterrupt:
+			output = subprocess.Popen([r"unrar","t", "-p%s" % ( pwd ), p_file], stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0]
+			if (output.find("All OK") != -1):
+				print 'found good password: %s' % ( pwd )
 				good_password = 1
-				sys.exit()
-			except Exception:
-				#Bleh
-				return
+			i += self.offset 
+			
 
 		return
 
@@ -76,20 +82,13 @@ class Status(Thread):
 		global count
 		global good_password
 		while (1):
-			try:
-				time.sleep(1)
-				if (good_password != None):
-					sys.exit()
-				print "Probing: '%s' %d/sec" % ( current_pwd, count )
-				count = 0
-			except KeyboardInterrupt:
-				good_password = 1
-				sys.exit()
-			except Exception:
-				#Bleh
-				return
-			
+                    time.sleep(1)
+                    if (good_password != None):
+                        sys.exit()
+                    print "Probing: '%s' %d/sec" % ( current_pwd, count )
+                    count = 0
 		return
+
 
 class ExitChecker(Thread):
 	def __init__(self):
@@ -103,19 +102,36 @@ class ExitChecker(Thread):
 				good_password = 1
 				sys.exit()
 
+class ArgumentParser:
+    def __init__(self):
+        self.args = sys.argv
+    def parse(self):
+        global p_type
+        global p_file
+        del self.args[0]
+        for index in range(len(self.args)):
+            if (self.args[index] == "-t"):
+                p_type = self.args[index+1]
+            if (self.args[index].lower().find(".rar") != -1):
+                p_file = self.args[index]
+        return
+
 if __name__ == "__main__":
-	count = 0
-	current_pwd = "0"
-	good_password = None
-	quitter = ExitChecker()
-	quitter.setDaemon(True)
-	quitter.start()
-	status = Status(quitter)
-	status.start()
-	crackers = []
-	cracker = RARCrack(2,1)
-	crackers.append(cracker)
-	cracker.start()
-	cracker = RARCrack(2,2)
-	crackers.append(cracker)
-	cracker.start()
+    argp = ArgumentParser()
+    argp.parse()
+
+    count = 0
+    current_pwd = "0"
+    good_password = None
+    quitter = ExitChecker()
+    quitter.setDaemon(True)
+    quitter.start()
+    status = Status(quitter)
+    status.start()
+    crackers = []
+    cracker = RARCrack(2,1)
+    crackers.append(cracker)
+    cracker.start()
+    cracker = RARCrack(2,2)
+    crackers.append(cracker)
+    cracker.start()
